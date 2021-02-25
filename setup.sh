8a7117c4cb81ecb413318e8f2eb271dce135e1cc
Phil Wilkins <mp3monster@users.noreply.github.com>
2021-02-25 23:54:05 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+echo fetching book resources
 mkdir fluent-logging
 cd fluent-logging
 mkdir simulator
@@ -11,13 +12,17 @@ wget https://github.com/mp3monster/LoggingInActionWithFluentd/archive/public.zip
 unzip public.zip
 rm public.zip
 cd ..
-#sudo yum -y install ruby
+
+echo setting up Ruby
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 rvm install ruby-2.7.2
 
+echo getting Fluentd
 gem install fluentd
+fluentd --version
 
+echo setting up Groovy - let Groovy resolve Java dependency
 # sudo yum -y install  jdk-11.0.10.x86_64
 sudo yum -y install groovy

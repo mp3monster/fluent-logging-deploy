367f796f23c24a14f1e0122ec11eee7f8b604960
Phil Wilkins <mp3monster@users.noreply.github.com>
2021-02-25 21:39:16 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+mkdir fluent-logging
+cd fluent-logging
+mkdir simulator
+mkdir book
+cd simulator 
+wget https://github.com/mp3monster/LogGenerator/archive/master.zip
+unzip master.zip .
+rm master.zip
+cd ../book
+wget https://github.com/mp3monster/LoggingInActionWithFluentd/archive/public.zip
+unzip public.zip .
+rm public.zip
